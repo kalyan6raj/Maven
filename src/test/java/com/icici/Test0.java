72633a70bb10f2bff7b472ee2c0895d83ab3d247
KalyannRaaz <kalyan7raj@gmail.com>
2021-04-09 09:20:59 +0530

    second commit

---
+++ src/test/java/com/icici/Test0.java
@@ -0,0 +1,20 @@
+package com.icici;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test0
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
